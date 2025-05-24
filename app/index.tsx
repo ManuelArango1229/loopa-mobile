546bf40c0121a26fd0ebdbe656fb2fa4c9b3009b
ManuelArango1229 <juan12292004@outlook.com>
2025-05-24 18:39:22 -0500

    Implementación del login en root

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,103 @@
-import { View, Text } from "react-native";
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+
+import * as SecureStore from "expo-secure-store";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Index = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    setEmail("");
+    setPassword("");
+    console.log("Email:", email);
+    console.log("Password:", password);
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("x-mobile-app", "true");
+
+    const raw = JSON.stringify({
+      email: email,
+      password: password,
+    });
+
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+
+    fetch("http://10.0.2.2:3000/api/users/login", requestOptions)
+      .then((response) => response.text())
+      .then((result) => {
+        const { accessToken, refreshToken, user } = JSON.parse(result);
+        SecureStore.setItem("refreshToken", refreshToken);
+        SecureStore.setItem("accessToken", accessToken);
+        AsyncStorage.setItem("user", JSON.stringify(user));
+      })
+      .catch((error) => console.log("error", error));
+  };
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text>hello world</Text>
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 60 : 0}
+      className="flex-1"
+    >
+      <ScrollView
+        className="dark:bg-darkbg bg-lightbg flex-1"
+        keyboardShouldPersistTaps="handled"
+        contentContainerStyle={{
+          alignItems: "center",
+          paddingTop: 100,
+          paddingBottom: 100,
+        }}
+      >
+        <Text className="text-4xl font-bold dark:text-primary mb-20">
+          LOOPA
+        </Text>
+
+        <View className="mb-8 w-80 items-center">
+          <Text className="text-lg dark:text-primarySoft mb-2">Email:</Text>
+          <TextInput
+            className="h-10 p-2 text-center border dark:border-r-border border-r-lightBorder rounded-full w-80 dark:text-primarySoft"
+            autoComplete="email"
+            keyboardType="email-address"
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+
+        <View className="mb-8 w-80 items-center">
+          <Text className="text-lg dark:text-primarySoft mb-2">Password:</Text>
+          <TextInput
+            className="h-10 p-2 text-center rounded-full w-80 border border-r-lightBorder dark:border-r-border dark:text-primarySoft"
+            secureTextEntry
+            autoComplete="password"
+            value={password}
+            onChangeText={setPassword}
+          />
+        </View>
+        <TouchableOpacity
+          className="w-80 bg-primary py-3 rounded-full items-center mt-8"
+          activeOpacity={0.7}
+          onPress={handleLogin}
+        >
+          <Text className="text-white font-bold text-lg">Login</Text>
+        </TouchableOpacity>
+
+        <View style={{ height: 200 }} />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,32 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkbg: "#1E1E1E",
+        lightbg: "#F5F5F5",
+
+        card: "#18181B",
+        lightCard: "#F8FAFC",
+
+        text: "#FFFFFF",
+        lightText: "#1A1A1A",
+
+        mutedText: "#A1A1AA",
+        lightMutedText: "#52525B",
+
+        border: "#3C2A59",
+        lightBorder: "#E4E4E7",
+
+        primary: "#8B5CF6",
+        lightPrimary: "#7C3AED",
+
+        primarySoft: "#A78BFA",
+        lightPrimarySoft: "#DDD6FE",
+
+        inputText: "#EDE9FE",
+      },
+    },
   },
   plugins: [],
 };
