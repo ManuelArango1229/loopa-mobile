0d0a6ee0370aaf77e6ae2a9dca422e1ac296a66b
ManuelArango1229 <juan12292004@outlook.com>
2025-05-30 23:23:38 -0500

    Add animated splash screen using Lottie animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,15 @@ import { Stack } from "expo-router";
 import "./global.css";
 import { StatusBar } from "expo-status-bar";
 import { View } from "react-native";
+import React, { useState } from "react";
+import SplashScreen from "./splashScreen";
 
 export default function RootLayout() {
+  const [isAppReady, setIsAppReady] = useState(false);
+
+  if (!isAppReady) {
+    return <SplashScreen onFinish={() => setIsAppReady(true)} />;
+  }
   return (
     <View className="flex-1">
       <StatusBar style="auto" translucent />
---
+++ app/splashScreen.tsx
@@ -0,0 +1,17 @@
+import LottieView from "lottie-react-native";
+import animation from "@/assets/lottie/AnimationDark.json";
+
+const SplashScreen = ({ onFinish = () => {} }: { onFinish: () => void }) => {
+  return (
+    <LottieView
+      source={animation}
+      onAnimationFinish={onFinish}
+      autoPlay
+      loop={false}
+      resizeMode="cover"
+      style={{ flex: 1, width: "100%", height: "100%" }}
+    />
+  );
+};
+
+export default SplashScreen;
